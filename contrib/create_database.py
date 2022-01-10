f5bdf3e8e2631c2231341b760ab27d80846a77a3
Alisson Andrade <54157447+AlisoSouza@users.noreply.github.com>
2022-01-10 20:03:39 +0000

    Update create_database.py

--- contrib/create_database.py
+++ contrib/create_database.py
@@ -1,7 +1,7 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-con = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="postgres")
+con = psycopg2.connect(host="postgres", database="postgres", user="postgres", password="postgres")
 con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = con.cursor()
 cur.execute("CREATE USER connect WITH PASSWORD 'connect'")
