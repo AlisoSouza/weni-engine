c4c04c3e87d024ebb80077233478a1ad06ddf1dc
Alisson <alissonfilipe937@gmail.com>
2022-01-11 18:09:09 +0000

    Add ci scripts

--- contrib/create_database.py
+++ contrib/create_database.py
@@ -1,11 +1,25 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-con = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="postgres")
-con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-cur = con.cursor()
-cur.execute("CREATE USER connect WITH PASSWORD 'connect'")
-cur.execute("ALTER ROLE connect WITH SUPERUSER")
-cur.execute("CREATE DATABASE connect")
-cur.close()
-con.close()
+def create_database():
+    try:
+        con = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="postgres")
+        con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    except Exception as e:
+        print(e)
+        raise Exception("Could not connect to database")
+
+    cur = con.cursor()
+
+    try:
+        cur.execute("CREATE USER connect WITH PASSWORD 'connect'")
+        cur.execute("ALTER ROLE connect WITH SUPERUSER")
+        cur.execute("CREATE DATABASE connect")
+    except Exception as e:
+        raise(e)
+    # close connection
+    cur.close()
+    con.close()
+
+if __name__ == "__main__":
+    create_database()
